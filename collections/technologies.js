d270109e0275c5b564ff9b7bee8abdab04586895
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-23 02:52:20 -0500

    Add Schema outline for technologies collection

--- collections/technologies.js
+++ collections/technologies.js
@@ -1,1 +1,10 @@
 Technologies = new Mongo.Collection('technologies');
+// Schema Overview
+// name -> Meteor -> String
+// language -> Javascript -> String
+// classification -> Full-Stack Javascript -> String
+// documentation -> Link to docs or interactive docs -> Array of links
+// articles -> any articles that have 'Meteor' tagged -> Array of articleId's
+// tutorials -> any tutorials that have 'Meteor' tagged -> Array of tutorialId's
+// videos -> any videos that have 'Meteor' tagged -> Array of videoId's
+// questions -> any questions that have 'Meteor' tagged -> Array of questionId's
